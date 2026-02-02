e25159f0fb25e908cad0cbc02c4b7c7ae04b2b19
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2026-02-02 13:55:27 +0530

    exe

---
+++ my-react-app/src/App1.js
@@ -0,0 +1,24 @@
+import React from 'react';
+class App1 extends React.Component{
+    constructor(){
+        super();
+        this.forceUpdatestate = this.forceUpdatestate.bind(this);
+    }
+    forceUpdatestate(){
+        this.forceUpdate();
+    }
+    render(){
+        return(
+            <div>
+                <h1>example to generate random Number</h1>
+                <h2>Random Number : {Math.random()}</h2>
+
+                <button onClick={this.forceUpdatestate}>Generate New Random Number</button>
+            </div>  
+
+        )
+
+    }
+}
+
+export default App1;
--- my-react-app/src/context.js
+++ my-react-app/src/context.js
@@ -12,3 +12,20 @@ function Profile() {
   const user = useContext(UserContext);
   return <h1>{user.name}</h1>;
 }
+
+
+function Layout() {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+}
+
+
+const user = {
+  name: 'Vino',
+  age: 20,
+  email: 'cvinodhini19992@gmail.com',
+  location: 'India'
+};
